8b9d0e540c242590bebec24946e776db79a1bcc4
Leandro Henriques <lehenriques@gmail.com>
2021-06-22 20:36:38 -0300

    Alterando para servidor enviar mensagem

--- client.c
+++ client.c
@@ -8,6 +8,8 @@
 #include <unistd.h>
 #include <string.h>
 
+#define PORT 8080
+
 void error(const char *msg)
 {
   perror(msg);
@@ -16,27 +18,28 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-  int sock, n;
+  int sock, n, m;
   unsigned int length;
   struct sockaddr_in server, from;
   struct hostent *hp;
   char buffer[256];
+  char buf[1024];
   
-  if (argc != 3) { 
-    printf("Usage: server port\n");
-    exit(1);
-  }
+  // if (argc != 3) { 
+  //   printf("Usage: server port\n");
+  //   exit(1);
+  // }
 
   sock= socket(AF_INET, SOCK_DGRAM, 0);
   if (sock < 0) error("socket");
 
   server.sin_family = AF_INET;
-  hp = gethostbyname(argv[1]);
+  hp = gethostbyname("127.0.0.1");
   if (hp==0) 
     error("Unknown host");
 
   bcopy((char *)hp->h_addr, (char *)&server.sin_addr, hp->h_length);
-  server.sin_port = htons(atoi(argv[2]));
+  server.sin_port = htons( PORT );
   length=sizeof(struct sockaddr_in);
   
   while (1) {
@@ -45,15 +48,16 @@ int main(int argc, char *argv[])
     fgets(buffer,255,stdin);
     
     n=sendto(sock,buffer,strlen(buffer),0,(const struct sockaddr *)&server,length);
-    if (n < 0) 
+    if (n < 0) {
       error("ERROR: enviar para");
-    
-    n = recvfrom(sock,buffer,256,0,(struct sockaddr *)&from, &length);
-    if (n < 0) 
-      error("ERROR: Receber de");
+    }else{
+      printf("Mensagem enviada...\n");
+    } 
 
-    write(1,"Resp. serivdor: ",16);
-    write(1,buffer,n);
+    m = recvfrom(sock,buffer,256,0,(struct sockaddr *)&from, &length);
+    if (m < 0) 
+      error("ERROR: Receber de");
+    write(1,buffer,m);
   }
   close(sock);
   return 0;
--- servidor.c
+++ servidor.c
@@ -7,6 +7,8 @@
 #include <netdb.h>
 #include <stdio.h>
 
+#define PORT 8080
+
 void error(const char *msg)
 {
   perror(msg);
@@ -15,41 +17,55 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-  int sock, length, n;
+  int sock, length, n, m;
   socklen_t fromlen;
   struct sockaddr_in server;
   struct sockaddr_in from;
+  char buffer[256];
   char buf[1024];
 
-  if (argc < 2) {
-    fprintf(stderr, "ERROR, no port provided\n");
-    exit(0);
-  }
+  // if (argc < 2) {
+  //   fprintf(stderr, "ERROR, no port provided\n");
+  //   exit(0);
+  // }
   
   sock=socket(AF_INET, SOCK_DGRAM, 0);
+
   if (sock < 0) error("Opening socket");
-  length = sizeof(server);
+    length = sizeof(server);
+
   bzero(&server,length);
+  
   server.sin_family=AF_INET;
   server.sin_addr.s_addr=INADDR_ANY;
-  server.sin_port=htons(atoi(argv[1]));
+  server.sin_port=htons( PORT );
+
   if (bind(sock,(struct sockaddr *)&server,length)<0) 
-    error("binding");
+    error("Erro ao fazer bind");
 
   fromlen = sizeof(struct sockaddr_in);
+
   while (1) {
     n = recvfrom(sock,buf,1024,0,(struct sockaddr *)&from,&fromlen);
-    
     if (n < 0) 
-      error("recvfrom");
+      error("ERROR: Receber de");
 
     write(1,"Mensagem recebida: ",19);
-    
     write(1,buf,n);
+
+    bzero(buffer,256);
+    fgets(buffer,255,stdin);
     
-    n = sendto(sock,"Sua mensagem foi recebida.\n",28,0,(struct sockaddr *)&from,fromlen);
-    if (n  < 0) 
+    m=sendto(sock,buffer,strlen(buffer),0,(const struct sockaddr *)&from,fromlen);
+    if (m < 0) {
       error("ERROR: enviar para");
+    }else{
+      printf("Mensagem enviada...\n");
+    }    
+    
+    // n = sendto(sock,"Sua mensagem foi recebida.\n",28,0,(struct sockaddr *)&from,fromlen);
+    // if (n  < 0) 
+    //   error("ERROR: enviar para");
   }
   return 0;
 }
